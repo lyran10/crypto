7adad7103dddc5a81b8c62893af6181903b6611a
lyran10 <liranramekar629@gmail.com>
2022-08-31 00:47:18 +0300

    added

--- Crypto-Currency/server.js
+++ Crypto-Currency/server.js
@@ -7,7 +7,7 @@ const router = require("./routes/routes.js");
 const path = require("path")
 dotenv.config();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://localhost:3001", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/", router);
