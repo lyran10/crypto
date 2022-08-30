f03696605f3476fb6c7ee98c0b5dad716a87b7c5
lyran10 <liranramekar629@gmail.com>
2022-08-31 00:25:46 +0300

    added

--- Crypto-Currency/client/src/index.js
+++ Crypto-Currency/client/src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import CryptoContext from "./cryptoContext";
-import "react-alice-carousel/lib/alice-carousel.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
